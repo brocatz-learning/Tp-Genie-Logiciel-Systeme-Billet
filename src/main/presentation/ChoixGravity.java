f5d75344a031c90a39ccd881f10bbcdafd96d076
brocatz <brocatzcat@gmail.com>
2022-06-05 21:35:29 -0400

    Changes

---
+++ src/main/presentation/ChoixGravity.java
@@ -0,0 +1,17 @@
+package presentation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChoixGravity {
+
+    /**
+     * Liste des choix disponible pour la gravite
+     * 1- Faible
+     * 2- Moyen
+     * 3- Elevee
+     */
+
+    public static final List<Integer> optionsGravity = Arrays.asList(1,2,3);
+}
+
